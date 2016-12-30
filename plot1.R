deb607abea0203862fd750bc000660f732ce142f
manansingh5 <manansingh5@gmail.com>
2016-12-30 14:42:40 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+initial <- file('./R/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(initial),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+
+png(filename = './R/plot1.png', width = 480, height = 480, units='px')
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red'))
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,9 @@
+initial <- file('./R/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(initial),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+png(filename = './R/plot2.png', width = 480, height = 480, units='px')
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+
+dev.off()
